d8d342d962a754e9de1d7956d52fe29d5e926b52
Delecroix <d-alexis@hotmail.fr>
2022-12-06 09:28:17 +0100

    Réglage du menu burger responsive

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
         $('.burger').find('.barre2').toggleClass('add');
 		$('.burger').find('.barre1').toggleClass('add');
 		$('.burger').find('.barre3').toggleClass('add');
-		// $('.burger').find('.barre').toggleClass('white');
+		 $('.burger').find('.barre').toggleClass('white');
 	})
 
 });
